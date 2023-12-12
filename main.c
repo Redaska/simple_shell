1a1b8d60a0a0fe43b0aa0be7dacf08144381f88f
Redaska <reda.essakali@gmail.com>
2023-12-12 21:05:21 +0100

    Add header files to main.h and Add boilerplate code to main.c file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @ac: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 when success 1 when error
+ */
+
+int main(int ac, char **argv)
+{
+    
+	return (0);
+}
